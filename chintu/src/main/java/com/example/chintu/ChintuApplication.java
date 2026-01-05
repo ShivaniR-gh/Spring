fa50ebf43db0a17e95cdca402a6b27a83cb922af
ShivaniR-gh <shivani53286@gmail.com>
2026-01-05 08:33:12 +0530

    initial commit

---
+++ chintu/src/main/java/com/example/chintu/ChintuApplication.java
@@ -0,0 +1,13 @@
+package com.example.chintu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChintuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChintuApplication.class, args);
+	}
+
+}
